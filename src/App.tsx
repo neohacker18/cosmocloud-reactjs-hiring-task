34f085631daf60bd7ae26071f5da2daeb9a7d352
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2023-09-23 12:23:56 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Select, Space } from "antd";
 
 interface Item {
   name: string;
@@ -31,51 +33,127 @@ const Field: React.FC<{
   setItemList: React.Dispatch<React.SetStateAction<Item[]>>;
 }> = ({ item, handleRemoveItem, handleAddItem, itemList, setItemList }) => {
   return (
-    <div style={{ marginLeft: `${item.parentMargin}px` }}>
-      <select
-        name=""
-        id=""
-        value={item.type}
-        onChange={(e) => {
-          const updatedItemList = [...itemList];
-          const itemToUpdate = findItemRecursively(updatedItemList, item.key);
-          if (itemToUpdate) {
-            itemToUpdate.type = e.target.value;
-            setItemList(updatedItemList);
-          }
+    <div
+      style={{
+        marginLeft: `${item.parentMargin}px`,
+        // border:'1px solid black'
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
         }}
       >
-        <option value="number">number</option>
-        <option value="string">string</option>
-        <option value="nested">nested</option>
-      </select>
-      {item.type === "nested" && (
-        <button onClick={() => handleAddItem(item.key, "number")}>
-          add item
-        </button>
-      )}
-      <button onClick={() => handleRemoveItem(item.key)}>remove</button>
-      <div>
-        {item.type === "nested" &&
-          item.children.map((childItem) => (
-            <Field
-              key={childItem.key}
-              item={childItem}
-              handleAddItem={handleAddItem}
-              handleRemoveItem={handleRemoveItem}
-              itemList={itemList}
-              setItemList={setItemList}
-            />
-          ))}
+        <Space style={{ display: "flex" }} align="baseline">
+          <Input
+            style={{ width: 200 }}
+            id={"InputField"}
+            value={item.name}
+            placeholder="Enter field"
+            onChange={(event) => {
+              const updatedItemList = [...itemList];
+              const itemToUpdate = findItemRecursively(
+                updatedItemList,
+                item.key
+              );
+              if (itemToUpdate) {
+                itemToUpdate.name = event.target.value;
+                setItemList(updatedItemList);
+              }
+            }}
+          />
+          <Select
+            style={{ width: 200 }}
+            value={item.type}
+            onChange={(value) => {
+              const updatedItemList = [...itemList];
+              const itemToUpdate = findItemRecursively(
+                updatedItemList,
+                item.key
+              );
+              if (itemToUpdate) {
+                itemToUpdate.type = value;
+                setItemList(updatedItemList);
+              }
+            }}
+          >
+            <option value="number">number</option>
+            <option value="string">string</option>
+            <option value="nested">nested</option>
+          </Select>
+          <Form.Item style={{ flex: 1 }}>
+            <Button danger onClick={() => handleRemoveItem(item.key)} block>
+              remove
+            </Button>
+          </Form.Item>
+        </Space>
       </div>
+      {item.type === "nested" &&
+        item.children.map((childItem) => (
+          <Field
+            key={childItem.key}
+            item={childItem}
+            handleAddItem={handleAddItem}
+            handleRemoveItem={handleRemoveItem}
+            itemList={itemList}
+            setItemList={setItemList}
+          />
+        ))}
+      {item.type === "nested" && (
+        <div>
+          <Form.Item style={{ maxWidth: `${530+item.parentMargin}px`,paddingLeft:`${item.parentMargin+30}px` }}>
+            <Button
+              type="primary"
+              onClick={() => handleAddItem(item.key, "number")}
+              block
+              icon={<PlusOutlined />}
+            >
+              Add Item
+            </Button>
+          </Form.Item>
+        </div>
+      )}
     </div>
   );
 };
+
 const App: React.FC = () => {
   const [itemList, setItemList] = useState<Item[]>([]);
   const [counter, setCounter] = useState<number>(0);
   const [option, setOption] = useState("number");
   const [isAddingItem, setIsAddingItem] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
+  const [jsonView, setJsonView] = useState<string>("");
+  const [status,setStatus]=useState<boolean>(false)
+
+  useEffect(() => {
+    updateJsonView();
+  }, [itemList]);
+  const processItem = (item) => {
+    if (item.type === "nested") {
+      const result = {
+        [item.name]: {}, 
+      };
+      for (const child of item.children) {
+        Object.assign(result[item.name], processItem(child));
+      }
+      return result;
+    } else {
+      return {
+        [item.name]: item.type,
+      };
+    }
+  };
+  
+  const updateJsonView = () => {
+    const result = {};
+    for (const item of itemList) {
+      Object.assign(result, processItem(item));
+    }
+    setJsonView(JSON.stringify(result, null, 2));
+  };
+  
 
   const handleAddItem = (parentKey: number | null, type: string): void => {
     if (isAddingItem) {
@@ -86,7 +164,7 @@ const App: React.FC = () => {
     let newItem: Item;
     if (parentKey === null) {
       newItem = {
-        name: "",
+        name: text,
         type,
         key: counter,
         parentKey: null,
@@ -100,22 +178,24 @@ const App: React.FC = () => {
       const parentItem = findItemRecursively(itemList, parentKey);
       if (parentItem) {
         newItem = {
-          name: "",
+          name: text,
           type,
           key: counter,
           parentKey,
           children: [],
-          parentMargin: parentItem.parentMargin + 10,
+          parentMargin: parentItem.parentMargin + 30,
         };
         parentItem.children.push(newItem);
       } else {
         setIsAddingItem(false);
         return;
       }
+      updateJsonView();
     }
 
     setCounter(counter + 1);
     setIsAddingItem(false);
+    setText("");
   };
 
   const handleRemoveItem = (key: number | null): void => {
@@ -126,39 +206,101 @@ const App: React.FC = () => {
     }
     setItemList(updatedItemList);
   };
+  useEffect(() => {
+    console.log(itemList);
+  }, [itemList]);
+  useEffect(()=>{
+    if(text!=="")
+    setStatus(false)
+  },[text])
 
   return (
     <>
-      {itemList &&
-        itemList.map((element) => (
-          <Field
-            key={element.key}
-            item={element}
-            handleAddItem={handleAddItem}
-            handleRemoveItem={handleRemoveItem}
-            itemList={itemList}
-            setItemList={setItemList}
-          />
-        ))}
-      <>
-        <input type="text" placeholder="Enter Field" />
-        <select
-          name=""
-          id=""
-          value={option}
-          onChange={(e) => setOption(e.target.value)}
+      <Form name="dynamic_form_nest_item" style={{ maxWidth: "100vw" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
         >
-          <option value="number">number</option>
-          <option value="string">string</option>
-          <option value="nested">nested</option>
-        </select>
-        <button
-          onClick={() => handleAddItem(null, option)}
-          disabled={isAddingItem}
-        >
-          add item
-        </button>
-      </>
+          {itemList &&
+            itemList.map((element) => (
+              <Field
+                key={element.key}
+                item={element}
+                handleAddItem={handleAddItem}
+                handleRemoveItem={handleRemoveItem}
+                itemList={itemList}
+                setItemList={setItemList}
+              />
+            ))}
+
+          <Space
+            align="baseline"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              maxWidth: 500,
+            }}
+          >
+            <Input
+              type="text"
+              required
+              status={status?"error":""}
+              value={text}
+              style={{ width: 200 }}
+              placeholder="Enter Field"
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+            />
+            <Select
+              name="select"
+              value={option}
+              onChange={(value) => setOption(value)}
+              style={{ width: 200 }}
+            >
+              <Option value="number">number</Option>
+              <Option value="string">string</Option>
+              <Option value="nested">nested</Option>
+            </Select>
+            <Form.Item style={{ flex: 1 }}>
+              <Button danger onClick={() => handleRemoveItem(0)} block>
+                remove
+              </Button>
+            </Form.Item>
+          </Space>
+          <Space
+            align="baseline"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              maxWidth: 500,
+            }}
+          >
+            <Form.Item style={{ flex: 1, width: 500 }}>
+              <Button
+                type="primary"
+                onClick={() => {
+                  if(text===""){
+                    setStatus(true)
+                    alert("Text missing")
+                    return;
+                  }
+                  setStatus(false)
+                  handleAddItem(null, option)
+                }}
+                block
+                style={{ width: "100%" }}
+                icon={<PlusOutlined />}
+              >
+                Add Item
+              </Button>
+            </Form.Item>
+          </Space>
+        </div>
+      </Form>
+      <pre>{jsonView}</pre>
     </>
   );
 };
